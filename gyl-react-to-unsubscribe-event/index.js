7208ae46f1e1d545be7807c5c21dc34e93e31f44
gyl-dev <gyl-dev@posteo.eu>
2020-02-02 23:11:04 +0100

    Clean up unsubscribe event listener

--- gyl-react-to-unsubscribe-event/index.js
+++ gyl-react-to-unsubscribe-event/index.js
@@ -1,69 +1,67 @@
-const dynamodb = require('dynopromise-client')
-const db = dynamodb()
+const AWS = require('aws-sdk');
 
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 const dbTablePrefix = process.env.DB_TABLE_PREFIX || '';
 
-const readMessageData = event => new Promise((resolve, reject) => {
+const getSubscriberByEmail = async email => {
+	const results = await dynamodb
+		.query({
+			TableName: `${dbTablePrefix}Subscribers`,
+			IndexName: 'EmailToStatusIndex',
+			KeyConditionExpression: '#email = :email',
+			ExpressionAttributeNames: {
+				'#email': 'email',
+			},
+			ExpressionAttributeValues: {
+				':email': email,
+			},
+		})
+		.promise();
+	if (!results.Count || !results.Items[0]) {
+		throw new Error('Subscriber does not exist.');
+	}
+	return results.Items[0];
+};
+
+exports.handler = async event => {
 	try {
-		return resolve(JSON.parse(event.Records[0].Sns.Message.trim()))
-	}
-	catch (err) {
-		return reject(err)
-	}
-})
-
-const getSubscriberByEmail = email => db.query({
-	TableName: `${dbTablePrefix}Subscribers`,
-	IndexName: 'EmailToStatusIndex',
-	KeyConditionExpression: '#email = :email',
-	ExpressionAttributeNames: {
-		'#email': 'email',
-	},
-	ExpressionAttributeValues: {
-		':email': email,
-	}
-})
-.then(results => {
-	if (!results.Count) {
-		throw new Error('Subscriber does not exist.')
-	}
-	return results.Items[0]
-})
-
-exports.handler = (event, context, callback) => {
-	readMessageData(event)
-	.then(message => {
-		const eventType = message.eventType
-		if (eventType === 'Complaint' || (eventType === 'Bounce' && message.bounce.bounceType === 'Permanent')) {
-			const detailName = eventType.toLowerCase()
-			const details = message[detailName]
-			let email = null
+		const message = JSON.parse(event.Records[0].Sns.Message);
+		const eventType = message.eventType;
+		if (
+			eventType === 'Complaint' ||
+			(eventType === 'Bounce' && message.bounce.bounceType === 'Permanent')
+		) {
+			const detailName = eventType.toLowerCase();
+			const details = message[detailName];
+			let email = null;
 			if (eventType === 'Complaint') {
-				email = details.complainedRecipients[0].emailAddress.toLowerCase()
-			}
-			else if (eventType === 'Bounce') {
-				email = details.bouncedRecipients[0].emailAddress.toLowerCase()
+				email = details.complainedRecipients[0].emailAddress.toLowerCase();
+			} else if (eventType === 'Bounce') {
+				email = details.bouncedRecipients[0].emailAddress.toLowerCase();
 			}
 			if (!email) {
-				throw new Error('Email not found in event')
+				throw new Error('Email not found in event');
 			}
-			return getSubscriberByEmail(email)
-			.then(subscriber => {
-				if (subscriber.unsubscribed) {
-					return
-				}
-				let reason = 'Other'
-				if (eventType === 'Complaint') {
-					reason = 'Complaint'
-				}
-				else if (eventType === 'Bounce' && message.bounce.bounceType === 'Permanent') {
-					reason = 'Bounce - Permanent'
-				}
-				Promise.all([
-					db.update({
+			const subscriber = await getSubscriberByEmail(email);
+			if (subscriber.unsubscribed) {
+				return;
+			}
+			let reason = 'Other';
+			if (eventType === 'Complaint') {
+				reason = 'Complaint';
+			} else if (
+				eventType === 'Bounce' &&
+				message.bounce.bounceType === 'Permanent'
+			) {
+				reason = 'Bounce - Permanent';
+			}
+			await Promise.all([
+				dynamodb
+					.update({
 						TableName: `${dbTablePrefix}Subscribers`,
 						Key: { subscriberId: subscriber.subscriberId },
-						UpdateExpression: 'set #unsubscribed = :true, #timestamp = :now, #unsubscribeReason = :reason',
+						UpdateExpression:
+							'set #unsubscribed = :true, #timestamp = :now, #unsubscribeReason = :reason',
 						ExpressionAttributeNames: {
 							'#unsubscribed': 'unsubscribed',
 							'#timestamp': 'unsubscribeTimestamp',
@@ -73,58 +71,67 @@ exports.handler = (event, context, callback) => {
 							':true': true,
 							':now': Date.now(),
 							':reason': reason,
-						}
-					}),
-					db.query({
+						},
+					})
+					.promise(),
+				dynamodb
+					.query({
 						TableName: `${dbTablePrefix}Queue`,
 						IndexName: 'subscriberId-index',
 						KeyConditionExpression: '#subscriberId = :subscriberId',
 						FilterExpression: '#queuePlacement = :queued',
 						ExpressionAttributeNames: {
 							'#subscriberId': 'subscriberId',
-							'#queuePlacement': 'queuePlacement'
+							'#queuePlacement': 'queuePlacement',
 						},
 						ExpressionAttributeValues: {
 							':subscriberId': subscriber.subscriberId,
-							':queued': 'queued'
+							':queued': 'queued',
 						},
 					})
-					.then(queueResults => {
+					.promise()
+					.then(async queueResults => {
 						if (!(queueResults.Items && queueResults.Items.length)) {
-							return
+							return;
 						}
-						const queueItems = queueResults.Items
-						const requests = []
+						const queueItems = queueResults.Items;
+						const requests = [];
 						queueItems.forEach(item => {
 							requests.push({
 								DeleteRequest: {
 									Key: {
 										queuePlacement: item.queuePlacement,
 										runAtModified: item.runAtModified,
-									}
-								}
-							})
-						})
-						const batchThreshold = 25
-						const batches = []
-						let currentBatch = []
+									},
+								},
+							});
+						});
+						const batchThreshold = 25;
+						const batches = [];
+						let currentBatch = [];
 						requests.forEach(request => {
 							if (currentBatch.length === batchThreshold) {
-								batches.push(currentBatch)
-								currentBatch = []
+								batches.push(currentBatch);
+								currentBatch = [];
 							}
-							currentBatch.push(request)
-						})
-						batches.push(currentBatch)
-						return Promise.all(batches.map(batch => db.batchWrite({
-							RequestItems: {
-								Queue: batch
-							}
-						})))
-					})
-				])
-			})
+							currentBatch.push(request);
+						});
+						batches.push(currentBatch);
+						await Promise.all(
+							batches.map(batch =>
+								dynamodb
+									.batchWrite({
+										RequestItems: {
+											Queue: batch,
+										},
+									})
+									.promise()
+							)
+						);
+					}),
+			]);
 		}
-	})
-	.catch(err => console.log(`Error handling event: ${err.message}`))
+	} catch (err) {
+		console.error(err);
+	}
 };
