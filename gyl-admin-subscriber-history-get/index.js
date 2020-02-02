65001dc7a8f14485eb2b15bfee33a8f146b2bd5c
gyl-dev <gyl-dev@posteo.eu>
2020-02-02 19:52:55 +0100

    Clean up subscriber history

--- gyl-admin-subscriber-history-get/index.js
+++ gyl-admin-subscriber-history-get/index.js
@@ -1,50 +1,39 @@
-const AWS = require('aws-sdk')
-const { fullQueryForDynamoDB } = require('full-query-for-dynamodb')
-const dynamodb = require('dynopromise-client')
+const AWS = require('aws-sdk');
+const { queryAllForDynamoDB } = require('query-all-for-dynamodb');
 
-const dbConfig = {
-	region: process.env.AWS_REGION,
-	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-}
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-const isLambda = !!process.env.AWS_EXECUTION_ENV
-
-const promiseDb = isLambda ? dynamodb() : dynamodb(dbConfig)
-const awsDb = isLambda ? (new AWS.DynamoDB.DocumentClient()) : (
-	new AWS.DynamoDB.DocumentClient(dbConfig)
-)
-
-exports.handler = async (event) => {
+exports.handler = async event => {
 	try {
-
-		// Light validation (just needs to be good enough for a non-sql query)
+		// Validate that a valid email is passed in.
 		if (
 			!event.queryStringParameters ||
-			(typeof event.queryStringParameters.email !== 'string') ||
-			(event.queryStringParameters.email.length > 256) ||
-			(event.queryStringParameters.email.length < 1)
+			typeof event.queryStringParameters.email !== 'string' ||
+			event.queryStringParameters.email.length > 256 ||
+			event.queryStringParameters.email.length < 1
 		) {
 			return {
 				statusCode: 400,
 				headers: { 'Access-Control-Allow-Origin': '*' },
 				body: 'Bad request',
-			}
+			};
 		}
 
 		// Get the subscriber, if they exist.
-		const { email } = event.queryStringParameters
-		const subscriberStatusResponse = await promiseDb.query({
-			TableName: 'Subscribers',
-			KeyConditionExpression: '#email = :email',
-			IndexName: 'EmailToStatusIndex',
-			ExpressionAttributeNames: {
-				'#email': 'email',
-			},
-			ExpressionAttributeValues: {
-				':email': email
-			}
-		})
+		const { email } = event.queryStringParameters;
+		const subscriberStatusResponse = await dynamodb
+			.query({
+				TableName: 'Subscribers',
+				KeyConditionExpression: '#email = :email',
+				IndexName: 'EmailToStatusIndex',
+				ExpressionAttributeNames: {
+					'#email': 'email',
+				},
+				ExpressionAttributeValues: {
+					':email': email,
+				},
+			})
+			.promise();
 
 		// Exit if there's no subscriber at this point.
 		if (
@@ -57,12 +46,12 @@ exports.handler = async (event) => {
 				statusCode: 404,
 				headers: { 'Access-Control-Allow-Origin': '*' },
 				body: 'Not Found',
-			}
+			};
 		}
 
 		// Fetch the history for the subscriber.
-		const { subscriberId } = subscriberStatusResponse.Items[0]
-		const history = await fullQueryForDynamoDB(awsDb, {
+		const { subscriberId } = subscriberStatusResponse.Items[0];
+		const historyResponse = await queryAllForDynamoDB(dynamodb, {
 			TableName: 'Queue',
 			IndexName: 'subscriberIdAndTagReason-index',
 			KeyConditionExpression: '#subscriberId = :subscriberId',
@@ -72,23 +61,23 @@ exports.handler = async (event) => {
 			ExpressionAttributeValues: {
 				':subscriberId': subscriberId,
 			},
-		})
-
-		history.sort((a, b) => b.runAt - a.runAt)
-		const itemDetails = await promiseDb.batchGet({
-			RequestItems: {
-				'Queue': {
-					Keys: history.slice(0, 100).map(item => {
-						const { queuePlacement, runAtModified } = item
-						return { queuePlacement, runAtModified }
-					})
-				}
-			}
-		})
-
-		const fullItems = (itemDetails.Responses.Queue || []).concat(history.slice(100))
-
-		// Return the result
+		});
+		let history = (historyResponse.Items || [])
+		history.sort((a, b) => b.runAt - a.runAt);
+		history = history.slice(0, 100)
+		const itemDetails = await dynamodb
+			.batchGet({
+				RequestItems: {
+					Queue: {
+						Keys: history.map(item => {
+							const { queuePlacement, runAtModified } = item;
+							return { queuePlacement, runAtModified };
+						}),
+					},
+				},
+			})
+			.promise();
+		const fullItems = (itemDetails.Responses.Queue || [])
 		return {
 			statusCode: 200,
 			headers: {
@@ -96,18 +85,13 @@ exports.handler = async (event) => {
 				'Content-Type': 'application/json',
 			},
 			body: JSON.stringify(fullItems),
-		}
-
-	}
-	catch (err) {
-
-		// Log any errors encountered and return server error if we don't know what
-		// went wrong.
-		console.error(err)
+		};
+	} catch (err) {
+		console.error(err);
 		return {
 			statusCode: 500,
 			headers: { 'Access-Control-Allow-Origin': '*' },
 			body: 'Server error',
-		}
+		};
 	}
-}
+};
