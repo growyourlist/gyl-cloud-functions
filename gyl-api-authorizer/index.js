db4adead01862069100dee07758340086aabf9ea
gyl-dev <gyl-dev@posteo.eu>
2020-02-03 20:48:26 +0100

    Authorizer handles diff header casing

--- gyl-api-authorizer/index.js
+++ gyl-api-authorizer/index.js
@@ -1,44 +1,37 @@
 const brcypt = require('bcrypt');
 
+const getPolicy = (methodArn, Effect) => {
+	return {
+		principalId: 'gyl-admin',
+		policyDocument: {
+			Version: '2012-10-17',
+			Statement: [
+				{
+					Action: 'execute-api:Invoke',
+					Effect,
+					Resource: `${methodArn
+						.split('/')
+						.slice(0, 1)
+						.join()}/*`,
+				},
+			],
+		},
+	}
+}
+
 exports.authorizer = async event => {
 	const headers = event.headers;
+	const authKey = headers['x-gyl-auth-key'] || headers['X-Gyl-Auth-Key']
+	if ((typeof authKey !== 'string') || (!authKey.trim())) {
+		return getPolicy(event.methodArn, 'Deny')
+	}
 	const match = await brcypt.compare(
-		headers['X-Gyl-Auth-Key'],
+		authKey,
 		process.env.ApiAuthKeyHash
 	);
 	if (match) {
-		return {
-			principalId: 'gyl-admin',
-			policyDocument: {
-				Version: '2012-10-17',
-				Statement: [
-					{
-						Action: 'execute-api:Invoke',
-						Effect: 'Allow',
-						Resource: `${event.methodArn
-							.split('/')
-							.slice(0, 1)
-							.join()}/*`,
-					},
-				],
-			},
-		};
+		return getPolicy(event.methodArn, 'Allow');
 	} else {
-		return {
-			principalId: 'gyl-admin',
-			policyDocument: {
-				Version: '2012-10-17',
-				Statement: [
-					{
-						Action: 'execute-api:Invoke',
-						Effect: 'Deny',
-						Resource: `${event.methodArn
-							.split('/')
-							.slice(0, 1)
-							.join()}/*`,
-					},
-				],
-			},
-		};
+		return getPolicy(event.methodArn, 'Deny')
 	}
 };
