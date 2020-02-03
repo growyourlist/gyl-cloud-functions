e8f0a2d6711fbf7a08061f19cbd2d669fb9358c0
gyl-dev <gyl-dev@posteo.eu>
2020-02-03 01:59:50 +0100

    Add policy doc to lambda authorizer

--- gyl-api-authorizer/index.js
+++ gyl-api-authorizer/index.js
@@ -1,4 +1,22 @@
 exports.authorizer = async (event) => {
-	console.log(event)
-	return true;
+	console.log(JSON.stringify(event))
+	const headers = event.headers
+	if (headers.Test === process.env.ApiAuthKeyHash) {
+		return {
+			principalId: 'me',
+			policyDocument: {
+				Version: '2012-10-17',
+				Statement: [
+					{
+						Action: 'execute-api:Invoke',
+						Effect: 'Allow',
+						Resource: event.methodArn
+					}
+				]
+			}
+		}
+	}
+	else {
+		return '401 Unauthorized'
+	}
 }
