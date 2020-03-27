4b84486515637b7f9da5f79bf8b3b3c61e9a31bc
gyl-dev <gyl-dev@posteo.eu>
2020-03-27 01:12:18 +0100

    Add postal address get

---
+++ gyl-admin-postal-address-get/index.js
@@ -0,0 +1,25 @@
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const dbTablePrefix = process.env.DB_TABLE_PREFIX || '';
+
+exports.handler = async () => {
+	try {
+		const dbResult = await dynamodb.get({
+			TableName: `${dbTablePrefix}Settings`,
+			Key: { settingName: 'postalAddress' },
+		}).promise();
+		return {
+			statusCode: 200,
+			headers: { 'Access-Control-Allow-Origin': '*' },
+			body: JSON.stringify((dbResult.Item && dbResult.Item.value) || ''),
+		};
+	}
+	catch (err) {
+		console.error(err)
+		return {
+			statusCode: 500,
+			headers: { 'Access-Control-Allow-Origin': '*' },
+			body: JSON.stringify(err.message),
+		}
+	}
+};
