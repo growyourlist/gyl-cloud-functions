dbf2e37d9ac141f868fc1bdfaa68b507460d55c9
gyl-dev <gyl-dev@posteo.eu>
2020-04-03 01:50:02 +0200

    Remove autoresponder from confirm get

--- gyl-public-subscriber-confirm-get/index.js
+++ gyl-public-subscriber-confirm-get/index.js
@@ -1,5 +1,4 @@
 const AWS = require('aws-sdk');
-const moment = require('moment-timezone');
 
 const dbTablePrefix = process.env.DB_TABLE_PREFIX || '';
 const dynamodb = new AWS.DynamoDB.DocumentClient();
@@ -22,26 +21,6 @@ const response = (statusCode, message = '') => {
 };
 
 /**
- * Based on the earliest given send data, user timezone and delivery preference,
- * return when the email should be sent.
- * @param  {Number} startSendAt When the email is scheduled to start sending.
- * @param  {String} timezone The timezone of the user.
- * @param  {Object} userDtp The user's delivery time preferences.
- * @return {Number} The time in milliseconds to attempt sending the email.
- */
-const getRunAtTime = (startSendAt, timezone, userDtp) => {
-	if (!timezone) {
-		return startSendAt;
-	}
-	const dtp = userDtp || { hour: 9, minute: 30 };
-	const sendAtTime = moment(startSendAt).tz(timezone);
-	sendAtTime.hour(dtp.hour);
-	sendAtTime.minute(dtp.minute);
-	sendAtTime.second(0);
-	return sendAtTime.valueOf();
-};
-
-/**
  * Gets a subscriber by their id.
  * @param  {String} subscriberId
  * @return {Promise<Object>}
@@ -55,28 +34,6 @@ const getSubscriber = subscriberId =>
 		.promise();
 
 /**
- * Creates a new queue item.
- */
-const newQueueItem = (itemData, runAt = Date.now()) => {
-	const realRunAt = getRunAtTime(
-		runAt,
-		itemData.subscriber.timezone,
-		itemData.subscriber.deliveryTimePreference
-	);
-	const runAtModified = `${realRunAt}${Math.random()
-		.toString()
-		.substring(1)}`;
-	return Object.assign({}, itemData, {
-		queuePlacement: 'queued',
-		runAtModified: runAtModified,
-		runAt: realRunAt,
-		attempts: 0,
-		failed: false,
-		completed: false,
-	});
-};
-
-/**
  * Sets the confirmed status of a subscriber to true.
  * @param  {Object} subscriberData
  * @return {Promise}
@@ -85,10 +42,12 @@ const confirmSubscriber = subscriberId =>
 	dynamodb
 		.update({
 			TableName: `${dbTablePrefix}Subscribers`,
-			Key: { subscriberId: subscriberId },
+			Key: { subscriberId },
+			ConditionExpression: 'attribute_exists(#subscriberId)',
 			UpdateExpression:
 				'set #confirmed = :true, #unsubscribed = :false, #confirmTimestamp = :timestamp',
 			ExpressionAttributeNames: {
+				'#subscriberId': 'subscriberId',
 				'#confirmed': 'confirmed',
 				'#unsubscribed': 'unsubscribed',
 				'#confirmTimestamp': 'confirmTimestamp',
@@ -101,68 +60,6 @@ const confirmSubscriber = subscriberId =>
 		})
 		.promise();
 
-const constructQueueBatch = (autoresponders, subscriber) => {
-	const batch = [];
-	autoresponders.forEach(ar => {
-		if (!ar.value.steps || !ar.value.steps.Start) {
-			return;
-		}
-		const startStep = ar.value.steps.Start;
-		if (startStep.type !== 'send email' || !startStep.templateId) {
-			return;
-		}
-		batch.push({
-			PutRequest: {
-				Item: newQueueItem({
-					type: startStep.type,
-					subscriber: subscriber,
-					subscriberId: subscriber.subscriberId,
-					templateId: startStep.templateId,
-					autoresponderId: ar.value.autoresponderId,
-					autoresponderStep: 'Start',
-				}),
-			},
-		});
-	});
-	return batch;
-};
-
-const runTriggeredAutoresponders = async subscriber => {
-	const triggeredAutoresponders = await dynamodb
-		.scan({
-			TableName: `${dbTablePrefix}Settings`,
-			FilterExpression:
-				'begins_with(#settingName, :autoresponder) and ' +
-				'#value.#trigger = :confirmed',
-			ExpressionAttributeNames: {
-				'#settingName': 'settingName',
-				'#value': 'value',
-				'#trigger': 'trigger',
-			},
-			ExpressionAttributeValues: {
-				':autoresponder': 'autoresponder-',
-				':confirmed': 'subscriber confirmed',
-			},
-		})
-		.promise();
-	if (!triggeredAutoresponders.Count) {
-		return;
-	}
-
-	const batch = constructQueueBatch(triggeredAutoresponders.Items, subscriber);
-	if (!batch.length > 0) {
-		return;
-	}
-
-	await dynamodb
-		.batchWrite({
-			RequestItems: {
-				[`${dbTablePrefix}Queue`]: batch,
-			},
-		})
-		.promise();
-};
-
 exports.handler = async event => {
 	try {
 		const subscriberId = event.queryStringParameters['t'];
@@ -173,7 +70,7 @@ exports.handler = async event => {
 		if (!result || !result.Item) {
 			return response(404, JSON.stringify('Not found'));
 		}
-		if (result.Item.confirmed === true && result.Item.unsubscribed === false) {
+		if (result.Item.confirmed && !result.Item.unsubscribed) {
 			return {
 				statusCode: 307,
 				headers: {
@@ -184,7 +81,6 @@ exports.handler = async event => {
 		}
 
 		await confirmSubscriber(subscriberId)
-		await runTriggeredAutoresponders(result.Item)
 		return {
 			statusCode: 307,
 			headers: {
