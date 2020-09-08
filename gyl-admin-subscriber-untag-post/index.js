53070125d772e639e91b299d300b47cd32647565
gyl-dev <gyl-dev@posteo.eu>
2020-09-08 10:59:58 +0200

    Bug fix - untag sub not working

--- gyl-admin-subscriber-untag-post/index.js
+++ gyl-admin-subscriber-untag-post/index.js
@@ -8,8 +8,12 @@ const dbTablePrefix = process.env.DB_TABLE_PREFIX || '';
 // Schema to validate the request to add a tag.
 const addTagSchema = Joi.object({
 	email: Joi.string().lowercase().email().required(),
-	tag: Joi.string().regex(/^[\w-]+$/).min(1).max(64).required(),
-})
+	tag: Joi.string()
+		.regex(/^[\w-]+$/)
+		.min(1)
+		.max(64)
+		.required(),
+});
 
 /**
  * Generates a response object with the given statusCode.
@@ -21,70 +25,70 @@ const response = (statusCode, body = '') => {
 		statusCode: statusCode,
 		headers: {
 			'Access-Control-Allow-Origin': '*',
-			'Content-Type': 'text/plain; charset=utf-8'
+			'Content-Type': 'text/plain; charset=utf-8',
 		},
 		body: body,
-	}
-}
+	};
+};
 
 /**
  * Fetches a subscriber id associated with the given email.
  * @param  {String} email
  * @return {Promise<Object>}
  */
-const getSubscriberByEmail = async email => {
-	const subscriberResponse = await dynamodb.query({
-		TableName: `${dbTablePrefix}Subscribers`,
-		IndexName: 'EmailToStatusIndex',
-		KeyConditionExpression: 'email = :email',
-		ExpressionAttributeValues: {
-			':email': email
-		},
-	}).promise()
+const getSubscriberByEmail = async (email) => {
+	const subscriberResponse = await dynamodb
+		.query({
+			TableName: `${dbTablePrefix}Subscribers`,
+			IndexName: 'EmailToStatusIndex',
+			KeyConditionExpression: 'email = :email',
+			ExpressionAttributeValues: {
+				':email': email,
+			},
+		})
+		.promise();
 	if (!subscriberResponse.Count) {
-		throw new Error('Subscriber not found')
+		throw new Error('Subscriber not found');
 	}
-	const subscriber = subscriberResponse.Items[0]
+	const subscriber = subscriberResponse.Items[0];
 	if (!subscriber) {
-		throw new Error('Subscriber not found')
+		throw new Error('Subscriber not found');
 	}
-	return subscriber
-}
+	return subscriber;
+};
 
 /**
  * Removes a tag from the subscriber with the given email address.
  */
 const removeTag = async (email, tag) => {
-	const subscriber = await getSubscriberByEmail(email)
-	const currentTags = subscriber.tags || []
-	const currentIndex = currentTags.indexOf(tag)
+	const subscriber = await getSubscriberByEmail(email);
+	const newTags = (subscriber.tags || []).slice();
+	const currentIndex = newTags.indexOf(tag);
 	if (currentIndex < 0) {
-		// Tag already exists, can return
-		return
+		// Tag does not exist, can return
+		return;
 	}
 
-	const newTags = currentTags.splice(currentIndex, 1)
-	return dynamodb.update({
-		TableName: `${dbTablePrefix}Subscribers`,
-		Key: { subscriberId: subscriber.subscriberId },
-		UpdateExpression: "set #tags = :tags",
-		ExpressionAttributeNames: { '#tags': 'tags' },
-		ExpressionAttributeValues: { ':tags': newTags },
-	})
-}
+	newTags.splice(currentIndex, 1);
+	return dynamodb
+		.update({
+			TableName: `${dbTablePrefix}Subscribers`,
+			Key: { subscriberId: subscriber.subscriberId },
+			UpdateExpression: 'set #tags = :tags',
+			ExpressionAttributeNames: { '#tags': 'tags' },
+			ExpressionAttributeValues: { ':tags': newTags },
+		})
+		.promise();
+};
 
-exports.handler = async event => {
+exports.handler = async (event) => {
 	try {
-		const addTagRequest = JSON.parse(event.body)
-		const addTagData = await addTagSchema.validateAsync(addTagRequest)
-		await removeTag(addTagData.email, addTagData.tag)
-		return response(200, JSON.stringify('OK'))
+		const addTagRequest = JSON.parse(event.body);
+		const addTagData = await addTagSchema.validateAsync(addTagRequest);
+		await removeTag(addTagData.email, addTagData.tag);
+		return response(200, JSON.stringify('OK'));
+	} catch (err) {
+		console.error(err);
+		return response(err.statusCode || 500, err.message || 'Error');
 	}
-	catch (err) {
-		console.error(err)
-		return response(
-			err.statusCode || 500,
-			err.message || 'Error',
-		)
-	}
-}
+};
