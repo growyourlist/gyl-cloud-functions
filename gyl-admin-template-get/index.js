fb60a60197ee2fd25b78a97793033cef5f58c582
gyl-dev <gyl-dev@posteo.eu>
2020-09-08 10:29:58 +0200

    Handle templates in old and new formats

--- gyl-admin-template-get/index.js
+++ gyl-admin-template-get/index.js
@@ -1,7 +1,7 @@
-const AWS = require('aws-sdk')
-const parse5 = require('parse5')
+const AWS = require('aws-sdk');
+const parse5 = require('parse5');
 
-const ses = new AWS.SES
+const ses = new AWS.SES();
 
 /**
  * Generates a response object with the given statusCode.
@@ -13,50 +13,71 @@ const response = (statusCode, body) => {
 	return {
 		statusCode: statusCode,
 		headers: {
-			'Access-Control-Allow-Origin': '*'
+			'Access-Control-Allow-Origin': '*',
 		},
-		body: JSON.stringify(body)
-	}
-}
+		body: JSON.stringify(body),
+	};
+};
 
-const getHtml = doc => {
-	try {
-		return parse5.serialize(doc.childNodes[1].childNodes[1].childNodes[0])
+const getHtml = (doc) => {
+	const body = doc.childNodes[1].childNodes[1];
+	if (body.childNodes.length > 1) {
+		return parse5.serialize(body);
 	}
-	catch (err) {
-		return ''
+	if (body.childNodes.length === 1) {
+		const firstBodyElm = body.childNodes[0];
+		if (firstBodyElm.tagName !== 'div') {
+			return parse5.serialize(body);
+		}
+		const styleAttr =
+			firstBodyElm.attrs &&
+			firstBodyElm.attrs.find((attr) => {
+				return attr.name === 'style';
+			});
+		if (!styleAttr) {
+			return parse5.serialize(body);
+		}
+		const isBackgroundDiv = styleAttr.value.startsWith(
+			'background:#ffffff;color:#000000'
+		);
+		if (isBackgroundDiv) {
+			return parse5.serialize(firstBodyElm);
+		}
+		return parse5.serialize(body);
 	}
-}
+	return '';
+};
 
 /**
  * Gets the SES email template.
  * @return {Promise}
  */
-const getTemplate = async templateName => {
-	const templateData = await ses.getTemplate({
-		TemplateName: templateName
-	}).promise()
-	const doc = parse5.parse(templateData.Template.HtmlPart)
+const getTemplate = async (templateName) => {
+	const templateData = await ses
+		.getTemplate({
+			TemplateName: templateName,
+		})
+		.promise();
+	const doc = parse5.parse(templateData.Template.HtmlPart);
 	return Object.assign({}, templateData.Template, {
 		PreviewPart: '',
 		HtmlPart: getHtml(doc),
-	})
-}
+	});
+};
 
-exports.handler = async event => {
+exports.handler = async (event) => {
 	try {
-		const templateName = event.queryStringParameters['templateName']
+		const templateName = event.queryStringParameters['templateName'];
 		if (!templateName) {
-			return response(400, null)
+			return response(400, null);
 		}
-		const template = await getTemplate(templateName)
-		return response(200, template)
-	}
-	catch (err) {
+		const template = await getTemplate(templateName);
+		return response(200, template);
+	} catch (err) {
 		if (err.name === 'TemplateDoesNotExist') {
-			return response(404, 'Not found')
+			return response(404, 'Not found');
 		}
-		console.error(err)
-		return response(500, err.message)
+		console.error(err);
+		return response(500, err.message);
 	}
-}
+};
