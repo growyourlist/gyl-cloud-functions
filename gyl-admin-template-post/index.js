4ae55a2c5b572dbd1ec2de63942048a20d1b3bb9
gyl-dev <gyl-dev@posteo.eu>
2020-09-08 10:40:33 +0200

    Save template version in history

--- gyl-admin-template-post/index.js
+++ gyl-admin-template-post/index.js
@@ -1,20 +1,20 @@
-const AWS = require('aws-sdk')
-const ses = new AWS.SES
+const AWS = require('aws-sdk');
+const ses = new AWS.SES();
+const db = new AWS.DynamoDB.DocumentClient();
 
 /**
  * Attempts to parse request body JSON.
  * @param  {String} body Request body
  * @return {Object}
  */
-const readTemplateData = body => {
+const readTemplateData = (body) => {
 	try {
-		return JSON.parse(body)
+		return JSON.parse(body);
+	} catch (ex) {
+		console.log(`Error reading request body: ${ex.message}`);
+		return null;
 	}
-	catch (ex) {
-		console.log(`Error reading request body: ${ex.message}`)
-		return null
-	}
-}
+};
 
 /**
  * Generates a response object with the given statusCode.
@@ -25,27 +25,29 @@ const response = (statusCode, body) => {
 	return {
 		statusCode: statusCode,
 		headers: {
-			'Access-Control-Allow-Origin': '*'
+			'Access-Control-Allow-Origin': '*',
 		},
 		body,
-	}
-}
+	};
+};
 
 const wrapHtmlPart = (subject, htmlBody) => {
-	return '<!DOCTYPE html>'
-	+ '<html lang="en">'
-	+ '<head>'
-	+ '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">'
-	+ `<title>${subject}</title>`
-	+ '<meta name="viewport" content="width=device-width,initial-scale=1">'
-	+ '</head>'
-  + '<body style="background:#ffffff;color:#000000;">'
-  + '<div style="background:#ffffff;color:#000000">'
-  +  htmlBody
-  + '</div>'
-	+ '</body>'
-	+ '</html>'
-}
+	return (
+		'<!DOCTYPE html>' +
+		'<html lang="en">' +
+		'<head>' +
+		'<meta http-equiv="Content-Type" content="text/html; charset=utf-8">' +
+		`<title>${subject}</title>` +
+		'<meta name="viewport" content="width=device-width,initial-scale=1">' +
+		'</head>' +
+		'<body style="background:#ffffff;color:#000000;">' +
+		'<div style="background:#ffffff;color:#000000">' +
+		htmlBody +
+		'</div>' +
+		'</body>' +
+		'</html>'
+	);
+};
 
 /**
  * Updates the template if it exists (i.e. TemplateName already exists) or
@@ -53,40 +55,45 @@ const wrapHtmlPart = (subject, htmlBody) => {
  * @param  {Object} params
  * @return {Promise}
  */
-const createOrUpdateTemplate = async params => {
+const createOrUpdateTemplate = async (params) => {
 	const templateParams = {
 		TemplateName: params.TemplateName,
 		SubjectPart: params.SubjectPart,
-		HtmlPart: wrapHtmlPart(
-			params.SubjectPart,
-			params.HtmlPart,
-		),
-		TextPart: params.TextPart
-	}
+		HtmlPart: wrapHtmlPart(params.SubjectPart, params.HtmlPart),
+		TextPart: params.TextPart,
+	};
 	try {
-		return await ses.createTemplate({Template: templateParams}).promise()
-	}
-	catch (err) {
+		await db
+			.put({
+				TableName: 'GylTemplateHistory',
+				Item: Object.assign({}, templateParams, {
+					Timestamp: Date.now(),
+				}),
+			})
+			.promise();
+		return await ses.createTemplate({ Template: templateParams }).promise();
+	} catch (err) {
 		if (err && err.code === 'AlreadyExists') {
-			return await ses.updateTemplate({
-				Template: templateParams
-			}).promise()
+			return await ses
+				.updateTemplate({
+					Template: templateParams,
+				})
+				.promise();
 		}
-		throw err
+		throw err;
 	}
-}
+};
 
-exports.handler = async event => {
+exports.handler = async (event) => {
 	try {
-		const templateData = readTemplateData(event.body)
+		const templateData = readTemplateData(event.body);
 		if (!templateData) {
-			return response(400, JSON.stringify('Bad request'))
+			return response(400, JSON.stringify('Bad request'));
 		}
-		await createOrUpdateTemplate(templateData)
-		return response(200, JSON.stringify('OK'))
+		await createOrUpdateTemplate(templateData);
+		return response(200, JSON.stringify('OK'));
+	} catch (err) {
+		console.log(`Error creating template: ${err.message}`);
+		return response(500, JSON.stringify(err.message));
 	}
-	catch (err) {
-		console.log(`Error creating template: ${err.message}`)
-		return response(500, JSON.stringify(err.message))
-	}
-}
+};
