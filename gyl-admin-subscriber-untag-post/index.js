d63295be5d8bc3328dfeebe31d320a8fd87a3c70
gyl-dev <gyl-dev@posteo.eu>
2020-10-15 15:28:29 +0200

    Add missing untag functionality

--- gyl-admin-subscriber-untag-post/index.js
+++ gyl-admin-subscriber-untag-post/index.js
@@ -1,12 +1,14 @@
 const AWS = require('aws-sdk');
 const Joi = require('@hapi/joi');
+const { queryAllForDynamoDB } = require('query-all-for-dynamodb');
+const { writeAllForDynamoDB } = require('write-all-for-dynamodb');
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 const dbTablePrefix = process.env.DB_TABLE_PREFIX || '';
 
 // Schema to validate the request to add a tag.
-const addTagSchema = Joi.object({
+const unTagSchema = Joi.object({
 	email: Joi.string().lowercase().email().required(),
 	tag: Joi.string()
 		.regex(/^[\w-]+$/)
@@ -63,14 +65,47 @@ const getSubscriberByEmail = async (email) => {
 const removeTag = async (email, tag) => {
 	const subscriber = await getSubscriberByEmail(email);
 	const newTags = (subscriber.tags || []).slice();
+
+	const queueResponse = await queryAllForDynamoDB(dynamodb, {
+		TableName: `${dbTablePrefix}Queue`,
+		IndexName: 'SubscriberIdIndex',
+		KeyConditionExpression: '#subscriberId = :subscriberId',
+		FilterExpression:
+			'#queuePlacement = :queued and (contains(#tagReason, :tag) or #tagReason = :tag)',
+		ExpressionAttributeNames: {
+			'#subscriberId': 'subscriberId',
+			'#queuePlacement': 'queuePlacement',
+			'#tagReason': 'tagReason',
+		},
+		ExpressionAttributeValues: {
+			':subscriberId': subscriber.subscriberId,
+			':queued': 'queued',
+			':tag': tag,
+		},
+	});
+	if (queueResponse.Items && queueResponse.Items.length) {
+		await writeAllForDynamoDB(dynamodb, {
+			RequestItems: {
+				[`${dbTablePrefix}Queue`]: queueResponse.Items.map((item) => ({
+					DeleteRequest: {
+						Key: {
+							queuePlacement: item.queuePlacement,
+							runAtModified: item.runAtModified,
+						},
+					},
+				})),
+			},
+		});
+	}
 	const currentIndex = newTags.indexOf(tag);
 	if (currentIndex < 0) {
-		// Tag does not exist, can return
+		// Tag does not exist, can return - double check that queue items have been cleared first
+		// though
 		return;
 	}
 
 	newTags.splice(currentIndex, 1);
-	return dynamodb
+	await dynamodb
 		.update({
 			TableName: `${dbTablePrefix}Subscribers`,
 			Key: { subscriberId: subscriber.subscriberId },
@@ -83,9 +118,9 @@ const removeTag = async (email, tag) => {
 
 exports.handler = async (event) => {
 	try {
-		const addTagRequest = JSON.parse(event.body);
-		const addTagData = await addTagSchema.validateAsync(addTagRequest);
-		await removeTag(addTagData.email, addTagData.tag);
+		const unTagRequest = JSON.parse(event.body);
+		const unTagData = await unTagSchema.validateAsync(unTagRequest);
+		await removeTag(unTagData.email, unTagData.tag);
 		return response(200, JSON.stringify('OK'));
 	} catch (err) {
 		console.error(err);
