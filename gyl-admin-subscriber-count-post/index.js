a0c6647d2fedbcd9b4f714b4d69bad258dc2d37c
gyl-dev <gyl-dev@posteo.eu>
2020-10-15 15:27:53 +0200

    Use new broadcast queue props

--- gyl-admin-subscriber-count-post/index.js
+++ gyl-admin-subscriber-count-post/index.js
@@ -2,84 +2,89 @@ const AWS = require('aws-sdk');
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const dbTablePrefix = process.env.DB_TABLE_PREFIX || '';
 
-exports.handler = async event => {
+exports.handler = async (event) => {
 	try {
-		let tags = null, properties = null, interactions = null, excludeTags = null
+		let tags = null,
+			properties = null,
+			interactions = null,
+			excludeTags = null,
+			ignoreConfirmed = null,
+			interactionWithAnyEmail = null;
 		try {
-			const input = JSON.parse(event.body)
+			const input = JSON.parse(event.body);
+			ignoreConfirmed = input.ignoreConfirmed;
+			interactionWithAnyEmail = input.interactionWithAnyEmail;
 			if (Array.isArray(input.tags)) {
-				input.tags.forEach(item => {
+				input.tags.forEach((item) => {
 					if (typeof item !== 'string') {
-						throw new Error('Invalid tags')
+						throw new Error('Invalid tags');
 					}
-				})
-				tags = input.tags
-			}
-			else {
-				tags = []
+				});
+				tags = input.tags;
+			} else {
+				tags = [];
 			}
 			if (Array.isArray(input.excludeTags)) {
-				input.excludeTags.forEach(item => {
+				input.excludeTags.forEach((item) => {
 					if (typeof item !== 'string') {
-						throw new Error('Invalid exclude tags')
+						throw new Error('Invalid exclude tags');
 					}
-				})
-				excludeTags = input.excludeTags
-			}
-			else {
-				excludeTags = []
+				});
+				excludeTags = input.excludeTags;
+			} else {
+				excludeTags = [];
 			}
 			if (typeof input.properties === 'object') {
-				Object.keys(input.properties).forEach(prop => {
+				Object.keys(input.properties).forEach((prop) => {
 					if (typeof input.properties[prop] !== 'string') {
-						throw new Error('Invalid properties')
+						throw new Error('Invalid properties');
 					}
-				})
-				properties = input.properties
-			}
-			else {
-				properties = null
+				});
+				properties = input.properties;
+			} else {
+				properties = null;
 			}
 			if (Array.isArray(input.interactions)) {
-				interactions = input.interactions
+				interactions = input.interactions;
+			} else {
+				interactions = null;
 			}
-			else {
-				interactions = null
-			}
-		}
-		catch (err) {
+		} catch (err) {
 			return {
 				statusCode: 400,
 				headers: { 'Access-Control-Allow-Origin': '*' },
 				body: 'Bad request',
-			}
+			};
 		}
-		await dynamodb.put({
-			TableName: `${dbTablePrefix}Settings`,
-			Item: {
-				settingName: 'previewSubscriberCount',
-				value: {
-					status: 'triggered',
-					count: 0,
-					tags,
-					excludeTags,
-					properties,
-					interactions,
-				}
-			}
-		}).promise()
+		await dynamodb
+			.put({
+				TableName: `${dbTablePrefix}Settings`,
+				Item: {
+					settingName: 'previewSubscriberCount',
+					value: {
+						status: 'triggered',
+						count: 0,
+						tags,
+						excludeTags,
+						properties,
+						interactions,
+						interactionWithAnyEmail,
+						ignoreConfirmed,
+					},
+				},
+			})
+			.promise();
 		return {
 			statusCode: 200,
 			headers: { 'Access-Control-Allow-Origin': '*' },
 			body: JSON.stringify('OK'),
-		}
-	}
-	catch (err) {
-		console.error(err)
+		};
+	} catch (err) {
+		console.error(err);
 		return {
 			statusCode: 500,
 			headers: { 'Access-Control-Allow-Origin': '*' },
-			body: JSON.stringify(err.message)
-		}
+			body: JSON.stringify(err.message),
+		};
 	}
-}
+};
