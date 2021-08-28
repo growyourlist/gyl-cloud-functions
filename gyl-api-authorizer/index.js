1e642370e6752129db4c8f74228bb715050ea00b
gyl-dev <gyl-dev@posteo.eu>
2021-08-28 11:17:20 +0200

    Add subscriber-only api key

--- gyl-api-authorizer/index.js
+++ gyl-api-authorizer/index.js
@@ -1,6 +1,6 @@
 const brcypt = require('bcrypt');
 
-const getPolicy = (methodArn, Effect) => {
+const getAdminPolicy = (methodArn, Effect) => {
 	return {
 		principalId: 'gyl-admin',
 		policyDocument: {
@@ -19,19 +19,51 @@ const getPolicy = (methodArn, Effect) => {
 	}
 }
 
+const getSubscribeOnlyPolicy = (methodArn, Effect) => {
+	return {
+		principalId: 'gyl-subscriber-only',
+		policyDocument: {
+			Version: '2012-10-17',
+			Statement: [
+				{
+					Action: 'execute-api:Invoke',
+					Effect,
+					Resource: methodArn
+				},
+			],
+		},
+	}
+}
+
 exports.authorizer = async event => {
 	const headers = event.headers;
-	const authKey = headers['x-gyl-auth-key'] || headers['X-Gyl-Auth-Key']
+	const authKey = headers['x-gyl-auth-key'] || headers['X-Gyl-Auth-Key'] || headers['X-GYL-AUTH-KEY'];
 	if ((typeof authKey !== 'string') || (!authKey.trim())) {
-		return getPolicy(event.methodArn, 'Deny')
+		console.log('deny: no key');
+		return getAdminPolicy(event.methodArn, 'Deny')
 	}
 	const match = await brcypt.compare(
 		authKey,
 		process.env.ApiAuthKeyHash
 	);
 	if (match) {
-		return getPolicy(event.methodArn, 'Allow');
+		console.log('allow: admin key');
+		return getAdminPolicy(event.methodArn, 'Allow');
 	} else {
-		return getPolicy(event.methodArn, 'Deny')
+		if (event.methodArn && event.methodArn === process.env.POST_SUBSCRIBER_ADMIN_ARN) {
+			const subscribeOnlyMatch = await brcypt.compare(
+				authKey,
+				process.env.ApiAuthSubscribeHashKey
+			);
+			if (subscribeOnlyMatch) {
+				console.log('allow: subscribe-only key')
+				return getSubscribeOnlyPolicy(event.methodArn, 'Allow');
+			} else {
+				console.log('deny: subscribe only key validation failed')
+				return getSubscribeOnlyPolicy(event.methodArn, 'Deny')
+			}
+		}
+		console.log('deny: admin key validation failed')
+		return getSubscribeOnlyPolicy(event.methodArn, 'Deny')
 	}
 };
